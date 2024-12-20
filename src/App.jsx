9d9a0f9dcc6fba31d4817d82ca279d038406b469
Pratik Shukla <pratikshukla121299@gmail.com.com>
2024-12-20 11:26:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import TextInputForm from "./components/TextInputForm/TextInputForm";
+import TextInputFormContainer from "./components/TextInputForm/TextInputFormContainer";
+
+function App(){
+  return(
+    <div>
+      <h1>Hello, Welcome to HangMan</h1>
+      
+      <TextInputFormContainer  />
+     
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/TextInputForm/TextInputForm.jsx
@@ -0,0 +1,58 @@
+import TextInput from "../TextInput/TextInput";
+import Button from "../Button/Button";
+
+function TextInputForm({ handleFormSubmit, handleTextInputChange, value, inputType, setInputType }){
+
+    return(
+        <form className="flex items-end" onSubmit={handleFormSubmit}> 
+            <div className="mr-2 flex-1">
+                 <TextInput 
+                    label= "Enter a word or phrase"
+                    type={inputType}
+                    value={value}
+                    onChange={handleTextInputChange}
+                 />
+            </div>
+                <Button 
+                    styleType='warning'
+                    text={inputType=='password' ? 'Show' : 'Hide'}
+                    onClickHandler={() =>
+                        {if(value !=''){
+                            setInputType(inputType=='password' ? 'text' : 'password')
+                            }
+
+                        }
+                    }
+                />
+            <div>
+
+            
+                
+                {value && 
+                <Button 
+                    styleType='warning'
+                    text={inputType=='password' ? 'Show' : 'Hide'}
+                    onClickHandler={() =>
+                        {if(value !=''){
+                            setInputType(inputType=='password' ? 'text' : 'password')
+                            }
+
+                        }
+                    }
+                /> }
+            
+               
+            </div>
+
+            <div className="flex">
+                <Button 
+                    text="OK"
+                    buttonType="submit"
+                /> 
+
+            </div>
+        </form>
+    )
+}
+
+export default TextInputForm;
